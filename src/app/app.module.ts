fd800fe4fb0ac0366ebbc5ea561d53f247849ae6
Vinh (envy) <vinhvv@peacesoft.net>
2019-03-11 19:43:21 +0700

    module package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PackageModule } from './package/package.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PackageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
