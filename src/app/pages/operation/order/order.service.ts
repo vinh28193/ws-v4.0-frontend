adf604b9dd339e1707e52c29335e712e592e760e
huyvd <huyvd@peacesoft.net>
2019-03-12 11:36:46 +0700

    fix

--- src/app/pages/operation/order/order.service.ts
+++ src/app/pages/operation/order/order.service.ts
@@ -3,7 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {ClientService} from '../../../core/service/client.service';
 
 @Injectable()
-export class OrderService extends ClientService{
+export class OrderService extends ClientService {
   constructor(http: HttpClient) {
     super(http);
   }
