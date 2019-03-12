0e1c2eec81e2c379286fb9e662729c7312311646
huyvd <huyvd@peacesoft.net>
2019-03-12 13:43:43 +0700

    fix

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -99,5 +99,4 @@ export class LoginComponent extends BaseComponent implements OnInit {
             }
         });
     }
-
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,27 +4,5 @@
 
 export const environment = {
   production: false,
-  API_REPORT_VN: 'http://admin.weshop.asia/service/',
-  API_URL_VN: 'http://weshop.beta.vn/api',
-  API_URL_ID: 'http://weshop.beta.id/api',
-  API_URL_US: 'http://weshop.beta.id/api',
-  API_URL_MY: 'http://weshop.beta.my/api',
-  API_URL_PH: 'http://weshop.beta.ph/api',
-  API_URL_TH: 'http://weshop.beta.th/api',
-
-  IMG_URL_VN: 'https://static-v3.weshop.asia',
-  IMG_URL_ID: 'https://static-v3.weshop.asia',
-  IMG_URL_US: 'https://static-v3.weshop.asia',
-  IMG_URL_MY: 'https://static-v3.weshop.asia',
-  IMG_URL_PH: 'https://static-v3.weshop.asia',
-  IMG_URL_TH: 'https://static-v3.weshop.asia',
-
-  IMG_URL_WH: 'https://static-v3.weshop.asia',
-  API_URL_BACKEND: 'http://weshop-v31.beta/api',
-  // 'API_URL_BACKEND':"http://139.59.97.47:9082/api",
-
-
   OAUTH_URL: 'http://weshop.v4.api/v1',
-  CLIENT_ID: 'testclient',
-  CLIENT_SECRET: 'testpass'
 };
