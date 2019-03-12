78db6cafc501993cbf7bab46aec7248c7245ebd8
huyvd <huyvd@peacesoft.net>
2019-03-12 11:27:58 +0700

    fix

--- src/app/pages/operation/shipment/shipment.component.ts
+++ src/app/pages/operation/shipment/shipment.component.ts
@@ -1,28 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import {ShipmentService} from './shipment.service';
-import {BaseComponent} from '../../../core/compoment/base.compoment';
 
 @Component({
   selector: 'app-shipment',
   templateUrl: './shipment.component.html',
   styleUrls: ['./shipment.component.css']
 })
-export class ShipmentComponent extends BaseComponent implements OnInit {
+export class ShipmentComponent  implements OnInit {
 
   constructor(private shipmentService: ShipmentService) {
-    super();
   }
   getProcessing = false;
   ngOnInit() {
   }
-
-  getlist(page = 1, manfest = null ) {
-  const fd = new FormData();
-    // fd.append('filter', JSON.stringify(this.filter));
-    this.shipmentService.getList('shipment/list', fd, this.currentPage, this.limit).subscribe(res => {
-
-    });
-
-  }
-
 }
