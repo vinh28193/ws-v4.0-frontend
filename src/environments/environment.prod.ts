843845a4961f42fbe400010b0daa3781eb30b5f5
huyvd <huyvd@peacesoft.net>
2019-03-13 14:31:45 +0700

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 
 export const environment = {
   production: true,
-  API_URL: 'http://weshop.v4.api/v1',
-  IMG_URL_WH: 'http://static.weshop.local',
-  API_URL_BACKEND: 'http://weshop.v4.api/v1',
-  OAUTH_URL: 'http://weshop.v4.api/v1',
+  API_URL: 'http://weshop-v4.back-end.local.vn/v1',
+  IMG_URL_WH: 'http://weshop-v4.back-end.local.vn/v1',
+  API_URL_BACKEND: 'http://weshop-v4.back-end.local.vn/v1',
+  OAUTH_URL: 'http://weshop-v4.back-end.local.vn/v1',
 };
