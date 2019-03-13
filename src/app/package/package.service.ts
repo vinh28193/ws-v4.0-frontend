2b3877d1ecce8380982ba0af12350516e1341157
Vinh (envy) <vinhvv@peacesoft.net>
2019-03-13 13:14:36 +0700

    pacage

--- src/app/package/package.service.ts
+++ src/app/package/package.service.ts
@@ -1,9 +1,20 @@
 import {Injectable} from '@angular/core';
 import {ClientService} from '../core/service/client.service';
+import {EncryptionService} from '../core/service/encryption.service';
+import {PopupService} from '../core/service/popup.service';
 
 @Injectable({
     providedIn: 'root'
 })
 export class PackageService extends ClientService {
 
+    constructor(
+                public encryption: EncryptionService,
+                public popup: PopupService) {
+        super(encryption);
+
+    }
+    getAllList(filter: any | undefined) {
+        return this.get('package/index', filter);
+    }
 }
