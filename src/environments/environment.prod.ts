fe0b0ac36f5f9e8bc0ddee8d172332f73b0abd00
Hoang Cong Phuc <phuchc@peacesoft.net>
2019-03-15 16:19:08 +0700

    add ingore

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-
-export const environment = {
-  production: true,
-  API_URL: 'http://weshop-v4.back-end.local.vn/1',
-  IMG_URL_WH: 'http://static.weshop.local',
-  API_URL_BACKEND: 'http://weshop-v4.back-end.local.vn/1',
-  OAUTH_URL: 'http://weshop-v4.back-end.local.vn/1',
-};
