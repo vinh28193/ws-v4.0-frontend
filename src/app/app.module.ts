fb0407b41fab1baec732954da63449155bfdc3d1
Vinh (envy) <vinhvv@peacesoft.net>
2019-03-18 10:35:53 +0700

    lauout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 
-import {AppRoutingComponent} from './app.routing.component';
+import {AppRoutingComponent} from './app-routing.component';
 import {SharedModule} from './shared/shared.module';
 
 import {Router} from '@angular/router';
--- src/app/module.component.ts
+++ src/app/module.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {OnInit} from '@angular/core';
 
-@Component({
-  selector: 'app-module',
-  templateUrl: './module/module.component.html',
-  styleUrls: ['./module/module.component.css']
-})
 export class ModuleComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
