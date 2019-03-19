0d003420fe06ba59e8db5af066905f463ba1dc37
Hoang Cong Phuc <phuchc@peacesoft.net>
2019-03-19 13:57:30 +0700

    update ENV again

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-
-export const environment = {
-  production: true,
-    API_URL: 'http://weshop-v4.back-end.local.vn/1',
-    IMG_URL_WH: 'http://weshop-v4.back-end.local.vn/1',
-    API_URL_BACKEND: 'http://weshop-v4.back-end.local.vn/1',
-    OAUTH_URL: 'http://weshop-v4.back-end.local.vn',
-};
