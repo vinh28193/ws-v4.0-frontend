8b89df1d1e28946a25ad0460f92d60d130d65676
Hoang Cong Phuc <phuchc@peacesoft.net>
2019-03-25 15:23:17 +0700

    update proudct Fee

--- src/app/operation/pipes/ProductFee.pipe.ts
+++ src/app/operation/pipes/ProductFee.pipe.ts
@@ -8,24 +8,34 @@ export class ProductFeePipe implements PipeTransform {
     transform(value: any) {
         switch (value) {
             case 'product_price_origin':
+            case 'product_price_origin ':
                 return 'Product Price';
             case 'tax_fee_origin':
+            case 'tax_fee_origin ':
                 return 'Tax Fee';
             case 'origin_shipping_fee':
+            case 'origin_shipping_fee ':
                 return 'Shipping Fee';
             case 'weshop_fee':
+            case 'weshop_fee ':
                 return 'Weshop Fee';
             case 'intl_shipping_fee':
+            case 'intl_shipping_fee ':
                 return 'Inter nationnal Shipping Fee';
             case 'custom_fee' :
+            case 'custom_fee ' :
                 return 'Customer Fee';
-            case 'packing_fee':
+            case 'packing_fee' :
+            case 'packing_fee ':
                 return 'Fee for packing packages';
             case 'inspection_fee':
+            case 'inspection_fee ':
                 return 'Inspection fees ';
             case 'insurance_fee':
+            case 'insurance_fee ':
                 return 'Insurance fees ';
             case 'vat_fee' :
+            case 'vat_fee ' :
                 return 'VAT Fee';
         }
 
