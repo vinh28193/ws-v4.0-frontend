a19e5d35ef8b8e2dda3e8e04fcd645e6514ccfb6
VienLV <VienLV@example.com>
2019-03-26 10:04:11 +0700

    change price

--- src/app/operation/order/order-detail/purchase-card/purchase-card.component.ts
+++ src/app/operation/order/order-detail/purchase-card/purchase-card.component.ts
@@ -37,4 +37,17 @@ export class PurchaseCardComponent implements OnInit, DoCheck {
             }
         });
     }
+
+    totalAmount(product) {
+        let total = 0;
+        total += product.price_purchase ? parseFloat(product.price_purchase) : 0;
+        total += product.us_ship_purchase ? parseFloat( product.us_ship_purchase) : 0;
+        total += product.us_tax_purchase ? parseFloat(product.us_tax_purchase) : 0;
+        total = product.quantityPurchase ? total * parseFloat(product.quantityPurchase) : 0;
+        return total;
+    }
+
+    changeAmount(product) {
+        product.paidToSeller = this.totalAmount(product);
+    }
 }
