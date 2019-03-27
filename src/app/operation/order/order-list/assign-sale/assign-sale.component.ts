1b29865cbefe20ff95ff33c96653594ac5843478
Vinh (envy) <vinhvv@peacesoft.net>
2019-03-27 15:47:51 +0700

    update

--- src/app/operation/order/order-list/assign-sale/assign-sale.component.ts
+++ src/app/operation/order/order-list/assign-sale/assign-sale.component.ts
@@ -5,32 +5,28 @@ import {PopupService} from '../../../../core/service/popup.service';
 import {FormBuilder} from '@angular/forms';
 
 @Component({
-  selector: 'app-assign-sale',
-  templateUrl: './assign-sale.component.html',
-  styleUrls: ['./assign-sale.component.css']
+    selector: 'app-assign-sale',
+    templateUrl: './assign-sale.component.html',
+    styleUrls: ['./assign-sale.component.css']
 })
 export class AssignSaleComponent extends OrderDataComponent implements OnInit {
-  @Input() orderId: any;
-  @Input() saleId: any;
-  @Input() saleEmail: any;
-  @Input() sale: any;
+    @Input() orderId: any;
+    @Input() saleSupport: any;
 
-  constructor(private orderService: OrderService, private popup: PopupService, private fb: FormBuilder) {
-    super(orderService);
-  }
-  assignSale() {
-    const put = this.orderService.createPostParams({
-      sale_support_id: this.saleId,
-    }, 'saleAssign')
-    this.orderService.put(`order/${this.orderId}`, put).subscribe(res => {
-      if (res.success) {
-        this.popup.success(res.message);
-      }
-        this.popup.error(res.message);
-    });
-  }
+    constructor(private orderService: OrderService, private popup: PopupService, private fb: FormBuilder) {
+        super(orderService);
+    }
 
-  ngOnInit() {
-  }
+    assignSale() {
+        this.orderService.get(`order/assign/${this.orderId}`, undefined).subscribe(res => {
+            if (res.success) {
+                this.popup.success(res.message);
+            }
+            this.popup.error(res.message);
+        });
+    }
+
+    ngOnInit() {
+    }
 
 }
