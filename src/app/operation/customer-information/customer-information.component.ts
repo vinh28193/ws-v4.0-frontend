21ac20275766c2a05dbf504298ffeba04c5ae52a
Vinh (envy) <vinhvv@peacesoft.net>
2019-04-04 09:53:41 +0700

    fix bug

--- src/app/operation/customer-information/customer-information.component.ts
+++ src/app/operation/customer-information/customer-information.component.ts
@@ -5,145 +5,149 @@ import {PopupService} from '../../core/service/popup.service';
 import {FormBuilder} from '@angular/forms';
 
 @Component({
-    selector: 'app-customer-information',
-    templateUrl: './customer-information.component.html',
-    styleUrls: ['./customer-information.component.css']
+  selector: 'app-customer-information',
+  templateUrl: './customer-information.component.html',
+  styleUrls: ['./customer-information.component.css']
 })
 export class CustomerInformationComponent extends OrderDataComponent implements OnInit, AfterContentChecked {
 
-    @Input() orderId: any = '';
-    @Input() name: any = '';
-    @Input() phone: any = '';
-    @Input() email: any = '';
-    @Input() address: any = '';
-    @Input() district: any = '';
-    @Input() districtName: any = '';
-    @Input() province: any = '';
-    @Input() provinceName: any = '';
-    @Input() country: any = '';
-    @Input() countryName: any = '';
-    @Input() postCode: any = '';
-    @Output() success: EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Input() orderId: any = '';
+  @Input() name: any = '';
+  @Input() phone: any = '';
+  @Input() email: any = '';
+  @Input() address: any = '';
+  @Input() district: any = '';
+  @Input() districtName: any = '';
+  @Input() province: any = '';
+  @Input() provinceName: any = '';
+  @Input() country: any = '';
+  @Input() countryName: any = '';
+  @Input() postCode: any = '';
+  @Output() success: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-    constructor(private orderService: OrderService, private popup: PopupService, private fb: FormBuilder) {
-        super(orderService);
+  constructor(private orderService: OrderService, private popup: PopupService, private fb: FormBuilder) {
+    super(orderService);
+  }
+
+  ngOnInit() {
+    console.log(this.orderId);
+    this.getCountries();
+    if (!this.isValidValue(this.country)) {
+      this.provinces = [];  // clear old provinces
+      this.districts = []; // clear old districts
+      const firstC = this.countries[0];
+      this.country = firstC.id;
+      this.countryName = firstC.name;
+      console.log(112);
     }
-
-    ngOnInit() {
-        console.log(this.orderId);
-        this.getCountries();
-        if (!this.isValidValue(this.country)) {
-            this.provinces = [];  // clear old provinces
-            this.districts = []; // clear old districts
-            const firstC = this.countries[0];
-            this.country = firstC.id;
-            this.countryName = firstC.name;
-            console.log(112);
-        }
-        this.getProvinces();
-        this.province = '';
-        this.provinceName = null;
-        if (this.provinces.length > 0) {
-            const firstP = this.provinces[0];
-            this.province = firstP.id;
-            this.provinceName = firstP.name;
-            this.getDistricts();
-            this.district = '';
-            this.districtName = null;
-            if (this.districts.length > 0) {
-                const firstD = this.districts[0];
-                this.district = firstD.id;
-                this.districtName = firstD.name;
-            }
-        }
+    this.getProvinces();
+    this.province = '';
+    this.provinceName = null;
+    if (this.provinces.length > 0) {
+      const firstP = this.provinces[0];
+      this.province = firstP.id;
+      this.provinceName = firstP.name;
+      this.getDistricts();
+      this.district = '';
+      this.districtName = null;
+      if (this.districts.length > 0) {
+        const firstD = this.districts[0];
+        this.district = firstD.id;
+        this.districtName = firstD.name;
+      }
     }
+  }
 
-    ngAfterContentChecked() {
+  ngAfterContentChecked() {
 
+  }
+
+
+  getInputUnique(attribute: string, unique?: string): string {
+    return attribute + (typeof unique !== 'undefined' ? unique : this.orderId);
+  }
+
+
+  updateReceiver() {
+    const post = this.orderService.createPostParams({
+      receiver_name: this.name,
+      receiver_phone: this.phone,
+      receiver_address: this.address,
+      receiver_country_id: this.country,
+      receiver_country_name: this.countryName,
+      receiver_province_id: this.province,
+      receiver_province_name: this.provinceName,
+      receiver_district_id: this.district,
+      receiver_district_name: this.districtName
+    }, 'updateReceiver');
+    this.orderService.put(`order/${this.orderId}`, post).subscribe(res => {
+      const rs: any = res;
+      this.success.emit(res.success);
+      if (rs.success) {
+        this.popup.success(res.message);
+      } else {
+        this.popup.error(res.message);
+      }
+    });
+  }
+
+  close() {
+    this.success.emit(true);
+  }
+
+  suggestionAddress(zip) {
+    // give full address by zip code
+    // see onCountry($event)
+  }
+
+  onCountry(country) {
+    const target = country.target;
+    this.country = target.value;
+    const selectedOptions = target.options;
+    const selectedIndex = selectedOptions.selectedIndex;
+    this.countryName = selectedOptions[selectedIndex].text;
+    this.provinces = [];  // clear old provinces
+    this.districts = []; // clear old districts
+    this.getProvinces();
+    this.province = '';
+    this.provinceName = null;
+    if (this.provinces.length > 0) {
+      const firstP = this.provinces[0];
+      this.province = firstP.id;
+      this.provinceName = firstP.name;
+      this.getDistricts();
+      this.district = '';
+      this.districtName = null;
+      if (this.districts.length > 0) {
+        const firstD = this.districts[0];
+        this.district = firstD.id;
+        this.districtName = firstD.name;
+      }
     }
+  }
 
-
-    getInputUnique(attribute: string, unique?: string): string {
-        return attribute + (typeof unique !== 'undefined' ? unique : this.orderId);
+  onProvince(province) {
+    const target = province.target;
+    this.province = target.value;
+    const selectedOptions = target.options;
+    const selectedIndex = selectedOptions.selectedIndex;
+    this.provinceName = selectedOptions[selectedIndex].text;
+    this.district = '';
+    this.districtName = null;
+    this.districts = []; // clear up
+    this.getDistricts();
+    if (this.districts.length > 0) {
+      const first = this.districts[0];
+      this.district = first.id;
+      this.districtName = first.name;
     }
+  }
 
-
-    updateReceiver() {
-        const post = this.orderService.createPostParams({
-            receiver_name: this.name,
-            receiver_phone: this.phone,
-            receiver_address: this.address,
-            receiver_country_id: this.country,
-            receiver_country_name: this.countryName,
-            receiver_province_id: this.province,
-            receiver_province_name: this.provinceName,
-            receiver_district_id: this.district,
-            receiver_district_name: this.districtName
-        }, 'updateReceiver');
-        this.orderService.put(`order/${this.orderId}`, post).subscribe(res => {
-            const rs: any = res;
-            this.success.emit(res.success);
-            if (rs.success) {
-                this.popup.success(res.message);
-            } else {
-                this.popup.error(res.message);
-            }
-        });
-    }
-
-    suggestionAddress(zip) {
-        // give full address by zip code
-        // see onCountry($event)
-    }
-
-    onCountry(country) {
-        const target = country.target;
-        this.country = target.value;
-        const selectedOptions = target.options;
-        const selectedIndex = selectedOptions.selectedIndex;
-        this.countryName = selectedOptions[selectedIndex].text;
-        this.provinces = [];  // clear old provinces
-        this.districts = []; // clear old districts
-        this.getProvinces();
-        this.province = '';
-        this.provinceName = null;
-        if (this.provinces.length > 0) {
-            const firstP = this.provinces[0];
-            this.province = firstP.id;
-            this.provinceName = firstP.name;
-            this.getDistricts();
-            this.district = '';
-            this.districtName = null;
-            if (this.districts.length > 0) {
-                const firstD = this.districts[0];
-                this.district = firstD.id;
-                this.districtName = firstD.name;
-            }
-        }
-    }
-
-    onProvince(province) {
-        const target = province.target;
-        this.province = target.value;
-        const selectedOptions = target.options;
-        const selectedIndex = selectedOptions.selectedIndex;
-        this.provinceName = selectedOptions[selectedIndex].text;
-        this.district = '';
-        this.districtName = null;
-        this.districts = []; // clear up
-        this.getDistricts();
-        if (this.districts.length > 0) {
-            const first = this.districts[0];
-            this.district = first.id;
-            this.districtName = first.name;
-        }
-    }
-
-    onDistrict(district) {
-        const target = district.target;
-        this.district = target.value;
-        const selectedOptions = target.options;
-        const selectedIndex = selectedOptions.selectedIndex;
-        this.districtName = selectedOptions[selectedIndex].text;
-    }
+  onDistrict(district) {
+    const target = district.target;
+    this.district = target.value;
+    const selectedOptions = target.options;
+    const selectedIndex = selectedOptions.selectedIndex;
+    this.districtName = selectedOptions[selectedIndex].text;
+  }
 }
--- src/app/operation/package/package-draft/package-draft.component.ts
+++ src/app/operation/package/package-draft/package-draft.component.ts
@@ -5,58 +5,65 @@ import {FormGroup} from '@angular/forms';
 import {MockResponse} from './mock-response';
 
 @Component({
-    selector: 'app-package-draft',
-    templateUrl: './package-draft.component.html',
-    styleUrls: ['./package-draft.component.css']
+  selector: 'app-package-draft',
+  templateUrl: './package-draft.component.html',
+  styleUrls: ['./package-draft.component.css']
 })
 export class PackageDraftComponent extends PackageDataComponent implements OnInit {
 
-    public items: any = [];
-    public item: any = {};
-    public itemForm: FormGroup;
-    public mapFrom: FormGroup;
-    public searchForm: FormGroup;
+  public items: any = [];
+  public item: any = {};
+  public itemForm: FormGroup;
+  public mapFrom: FormGroup;
+  public searchForm: FormGroup;
 
-    constructor(public draftPackageService: PackageDraftService) {
-        super(draftPackageService);
-    }
+  constructor(public draftPackageService: PackageDraftService) {
+    super(draftPackageService);
+  }
 
-    ngOnInit() {
-        this.currentPage = 1;
-        this.perPage = 20;
-        this.buildSearchForm();
-        this.buildItemForm();
-        this.buildMapForm();
-        this.search();
-        console.log(this.items);
-    }
+  ngOnInit() {
+    this.currentPage = 1;
+    this.perPage = 20;
+    this.buildSearchForm();
+    this.buildItemForm();
+    this.buildMapForm();
+    this.search();
+    console.log(this.items);
+  }
 
-    isUndefined($item) {
-        return !(
-            this.draftPackageService.isValidValue($item.order) &&
-            this.draftPackageService.isValidValue($item.product)
-        );
-    }
+  isUndefined($item) {
+    return !(
+      this.draftPackageService.isValidValue($item.order) &&
+      this.draftPackageService.isValidValue($item.product)
+    );
+  }
 
-    buildSearchForm() {
+  buildSearchForm() {
 
-    }
+  }
 
-    buildItemForm() {
+  buildItemForm() {
 
-    }
+  }
 
-    buildMapForm() {
+  buildMapForm() {
 
-    }
+  }
 
-    search() {
-        const data: any = MockResponse.data;
-        this.items = data._items;
-        this.totalCount = data._meta.totalCount;
-        this.pageCount = data._meta.pageCount;
-        this.currentPage = data._meta.currentPage;
-        this.perPage = data._meta.perPage;
-    }
+  search() {
+    const data: any = MockResponse.data;
+    this.items = data._items;
+    this.totalCount = data._meta.totalCount;
+    this.pageCount = data._meta.pageCount;
+    this.currentPage = data._meta.currentPage;
+    this.perPage = data._meta.perPage;
+  }
 
+  handlePagination(event) {
+
+  }
+
+  handlePerPage(event) {
+
+  }
 }
