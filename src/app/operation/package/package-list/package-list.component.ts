6711c89f3eec3950df6ad95f468af842cb9a2e54
VienLV <VienLV@example.com>
2019-04-27 15:03:30 +0700

    fix pub

--- src/app/operation/package/package-list/package-list.component.ts
+++ src/app/operation/package/package-list/package-list.component.ts
@@ -1,11 +1,12 @@
 import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
-import {BaseComponent} from '../../../core/base.compoment';
 
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {PackageService} from '../package.service';
 import {PopupService} from '../../../core/service/popup.service';
-import {BsDaterangepickerConfig} from 'ngx-bootstrap';
+import {ModalDirective} from 'ngx-bootstrap';
 import {PackageDataComponent} from '../package-data.component';
+import {DomSanitizer} from '@angular/platform-browser';
+import {ScopeService} from '../../../core/service/scope.service';
 
 @Component({
     selector: 'app-package-list',
@@ -14,87 +15,218 @@ import {PackageDataComponent} from '../package-data.component';
 })
 export class PackageListComponent extends PackageDataComponent implements OnInit {
 
-    @ViewChild('packageListTemplate') packageListTemplate: TemplateRef<any>;
-    @ViewChild('orderListTemplate') orderListTemplate: TemplateRef<any>;
-    @ViewChild('productListTemplate') productListTemplate: TemplateRef<any>;
 
-    public packages: any = [];
-    // form Group
+    @ViewChild('usSendingModal') usSendingModal: ModalDirective;
+    @ViewChild('mergeTracking') mergeTracking: ModalDirective;
+    @ViewChild('sellerRefundModal') sellerRefundModal: ModalDirective;
+    @ViewChild('updateModal') updateModal: ModalDirective;
+
+    public tracks: any = [];
+    public manifests: any = [];
+    public productIds: any = [];
+    public trackingT = '';
+    public trackingE = '';
+    public trackingMergeSearch = '';
+    public tabTracking = 'tracking';
+    public trackingCodes: any = [];
+    public p_e = 1;
+    public p_t = 1;
+    public l_e = 20;
+    public l_t = 20;
+    public limit_page = 9;
+    public manifest_id = '';
+    public filter_t = '';
+    public filter_e = '';
+    public trackingMerge: any = {
+        data_id: '',
+        ext_id: '',
+        data_tracking_code: '',
+        ext_tracking_code: '',
+        type: '',
+    };
+    public sellerRefundForm: any = {
+        id: '',
+        tracking: '',
+        type: 'full',
+        amount: 0.00,
+        time: '',
+    };
+    public updateForm: any = {
+        id: '',
+        tracking: '',
+        order_id: '',
+        product_id: '',
+        purchase_invoice_number: '',
+        item_name: '',
+        type: 'data_draft',
+    };
+    // file
+    public file: File;
+    // form
+    public usSendingForm: FormGroup;
     public searchForm: FormGroup;
-    // Template
-    public togglePackageTemplate: TemplateRef<any>;
 
-    public dateTime: Date;
-    public bsRangeValue: Date[];
-    public bsConfig: BsDaterangepickerConfig;
-
-    constructor(public packageService: PackageService, private popup: PopupService, private fb: FormBuilder) {
-        super(packageService);
+    constructor(
+        public packageService: PackageService,
+        public popUp: PopupService,
+        public fb: FormBuilder,
+        public sanitizer: DomSanitizer,
+        public _scope: ScopeService
+    ) {
+        super(packageService, _scope);
     }
 
     ngOnInit() {
         this.currentPage = 1;
         this.perPage = 20;
-        this.dateTime = new Date();
-        const maxDateTime: Date = this.dateTime;
-        maxDateTime.setDate(this.dateTime.getDate() + 1);
-        this.bsRangeValue = [this.dateTime, maxDateTime];
         this.buildSearchForm();
+        this.buildUsSendingForm();
         this.search();
-        this.togglePackageTemplate = this.packageListTemplate;
-    }
-
-    prepareSearch() {
-        const value = this.searchForm.value;
-
-        const params: any = {
-            perPage: value.perPage,
-            page: value.page
-        };
-        const filter = [];
-        if (value.keyWord !== '') {
-            const keywordDeep = {key: 'keyWord', value: {key: value.keyCategory, value: value.keyWord}};
-            filter.push(keywordDeep);
-        }
-        if (value.currentStatus !== 'ALL') {
-            filter.push({key: 'currentStatus', value: value.currentStatus});
-        }
-        if (value.timeRange.length > 0 && (value.timeRange[0] !== '' || value.timeRange[1] !== '')) {
-
-        }
-        if (filter.length > 0) {
-            const object = filter.reduce((obj, item) => Object.assign(obj, {[item.key]: item.value}), {});
-            params.filter = JSON.stringify(object);
-        }
-        return params;
-    }
-
-    search() {
-        const params = this.prepareSearch();
-        this.packageService.getAllList(params).subscribe(response => {
-            if (response.success) {
-                const data: any = response.data;
-                this.packages = data._items;
-                this.totalCount = data._meta.totalCount;
-                this.pageCount = data._meta.pageCount;
-                this.currentPage = data._meta.currentPage;
-                this.perPage = data._meta.perPage;
-            } else {
-                this.popup.error(response.message);
-            }
-        });
     }
 
     buildSearchForm() {
         this.searchForm = this.fb.group({
-            keyCategory: 'ALL',
-            keyWord: '',
-            timeKey: 'create_at',
-            timeRange: this.bsRangeValue,
-            currentStatus: 'ALL',
+            q: '',
             page: this.currentPage,
             perPage: this.perPage,
         });
+    }
+
+    buildUsSendingForm() {
+        this.usSendingForm = this.fb.group({
+            store: [''],
+            manifest: [''],
+            warehouse: [''],
+            date: '',
+            file: ['']
+        });
+    }
+
+    get store(): string {
+        return this.usSendingForm.get('store').value;
+    }
+
+    get listWarehouses(): any {
+        const store = this.store;
+        let listWarehouses = [
+            {key: this.allKey, value: '--select store first--'}
+        ];
+        if (store === '') {
+            listWarehouses = [
+                {key: this.allKey, value: '--select store first--'}
+            ];
+        } else if (store === 'vn') {
+            listWarehouses = [
+                {key: 'BMVN_HN', value: 'Boxme Ha Noi (Nam tu liem)'},
+                {key: 'BMVN_HCM', value: 'Boxme HCM (45 tan son)'},
+            ];
+        } else if (store === 'id') {
+            listWarehouses = [
+                {key: 'BMID_JKT', value: 'Boxme INDO (Jakata)'},
+            ];
+        } else {
+            listWarehouses = [
+                {key: this.allKey, value: '--select warehouse--'},
+                {key: 'BMVN_HN', value: 'Boxme Ha Noi (Nam tu liem)'},
+                {key: 'BMVN_HCM', value: 'Boxme HCM (45 tan son)'},
+                {key: 'BMID_JKT', value: 'Boxme INDO (Jakata)'},
+            ];
+        }
+        this.usSendingForm.patchValue({warehouse: listWarehouses[0].key});
+        return listWarehouses;
+    }
+
+    public handleFileChange(event) {
+        this.file = event.target.files[0];
+    }
+
+    public preCreate() {
+        const value = this.usSendingForm.getRawValue();
+        if (typeof this.file === 'undefined') {
+            this.popUp.error('no file update ?');
+        }
+        // return {
+        //     store: value.store,
+        //     warehouse: value.warehouse,
+        //     manifest: value.manifest,
+        //     file: this.file
+        // };
+        const fd = new FormData();
+        fd.append('store', value.store);
+        fd.append('warehouse', value.warehouse);
+        fd.append('manifest', value.manifest);
+        fd.append('file', this.file);
+        fd.append('date', value.date);
+        return fd;
+    }
+
+    public create() {
+        this.packageService.create(this.preCreate()).subscribe(res => {
+            const rs: any = res;
+            if (rs.success) {
+                this.popUp.success(rs.message);
+                this.usSendingModal.hide();
+            } else {
+                this.popUp.error(rs.message);
+                this.buildUsSendingForm();
+            }
+        });
+    }
+
+    public preSearch() {
+        const value = this.searchForm.value;
+
+        const params: any = {
+            t_t: this.trackingT,
+            t_e: this.trackingE,
+            m: this.manifest_id,
+            ps_t: this.l_t,
+            ps_e: this.l_e,
+            p_t: this.p_t,
+            p_e: this.p_e,
+            f_t: this.filter_t,
+            f_e: this.filter_e,
+            ps_m: value.perPage,
+            p_m: value.page
+        };
+        if (value.q !== '') {
+            params.q = value.q;
+        }
+        return params;
+    }
+
+    search(type = 'search', id = null) {
+        if (id !== this.manifest_id) {
+            this.manifest_id = id;
+            const params = this.preSearch();
+            if (type === 'search') {
+                params.m = '';
+                this.manifest_id = '';
+            }
+            this.packageService.searchUsSending(params).subscribe(response => {
+                const rs: any = response;
+                if (rs.success) {
+                    const data: any = rs.data;
+                    this.tracks = data._items;
+                    if (type === 'search') {
+                        this.manifests = data._manifest ? data._manifest : this.manifests;
+                    }
+                    this.tracks = data;
+                    this.totalCount = data._manifest_total ? data._manifest_total : this.totalCount;
+                    this.pageCount = Math.floor(this.totalCount / params.ps_m);
+                    this.currentPage = params.p_m;
+                    this.perPage = params.ps_m;
+                    this.setTabTracking(this.tabTracking);
+                } else {
+                    this.popUp.error(rs.message);
+                }
+            });
+        }
+    }
+
+    openUsSendingModal() {
+        this.buildUsSendingForm();
+        this.usSendingModal.show();
     }
 
     handlePagination(event) {
@@ -107,5 +239,157 @@ export class PackageListComponent extends PackageDataComponent implements OnInit
         const value = event.target.value;
         this.searchForm.patchValue({perPage: value});
         this.search();
+    }
+
+    merge() {
+        const listCheck = this.getExtTrackings('tracking');
+        if (!listCheck || listCheck === []) {
+            return this.popUp.error('Cannot find tracking code target!');
+        }
+        this.packageService.popup.confirm(() => {
+            this.packageService.post('s-us-send', this.trackingMerge).subscribe(rs => {
+                const res: any = rs;
+                if (res.success) {
+                    this.popUp.success(res.message);
+                    this.search('tracking');
+                    this.clearMerge();
+                    this.mergeTracking.hide();
+                } else {
+                    this.popUp.error(res.message);
+                }
+            });
+        }, this.trackingMerge.data_tracking_code + ' merge ' + this.trackingMerge.ext_tracking_code, 'Merge');
+    }
+
+    clearMerge() {
+        this.trackingMerge = {
+            data_id: '',
+            ext_id: '',
+            data_tracking_code: '',
+            ext_tracking_code: '',
+            type: '',
+        };
+    }
+
+    showSellerRefundModal(packTr) {
+        this.sellerRefundForm.id = packTr.id;
+        this.sellerRefundForm.tracking = packTr.tracking_code;
+        this.sellerRefundModal.show();
+    }
+
+    updateSellerRefund() {
+        if (!this.sellerRefundForm.amount || !this.sellerRefundForm.time) {
+            return this.popUp.error('All field cannot null!');
+        }
+        this.packageService.sellerRefundUsSending(this.sellerRefundForm.id, this.sellerRefundForm).subscribe(rs => {
+            const res: any = rs;
+            if (res.success) {
+                this.popUp.success(res.message);
+                this.search('tracking');
+                this.sellerRefundModal.hide();
+            } else {
+                this.popUp.error(res.message);
+            }
+        });
+    }
+
+    showUpdateForm(packTr) {
+        this.updateForm.id = packTr.id;
+        this.updateForm.item_name = packTr.item_name;
+        this.updateForm.order_id = packTr.order_id;
+        this.updateForm.product_id = packTr.product_id;
+        this.updateForm.purchase_invoice_number = packTr.purchase_invoice_number;
+        this.updateForm.tracking = packTr.tracking_code;
+        this.updateModal.show();
+    }
+
+    update() {
+        if (!this.updateForm.purchase_invoice_number ||
+            !this.updateForm.product_id || !this.updateForm.order_id || !this.updateForm.item_name) {
+            return this.popUp.error('All field cannot null!');
+        }
+        this.packageService.put('us-sending/' + this.updateForm.id, this.updateForm).subscribe(rs => {
+            const res: any = rs;
+            if (res.success) {
+                this.popUp.success(res.message);
+                this.updateModal.hide();
+                this.search('tracking');
+            } else {
+                this.popUp.error(res.message);
+            }
+        });
+    }
+
+    setTabTracking(tab) {
+        if (tab === 'ext') {
+            this.trackingCodes = this.tracks._ext;
+            this.tabTracking = tab;
+        } else {
+            this.trackingCodes = this.tracks._tracking;
+            this.tabTracking = 'tracking';
+        }
+    }
+
+    regetType(id) {
+        this.packageService.reGetType(id, {}).subscribe(rs => {
+            if (rs.success) {
+                this.popUp.success(rs.message);
+                // this.search();
+            } else {
+                this.popUp.error(rs.message);
+            }
+        });
+    }
+
+    searchEvent(event) {
+        let fil = '';
+        if (!event.filter.order_code || !event.filter.sku || !event.filter.tracking_code || event.filter.type_tracking) {
+            fil = window.btoa(JSON.stringify(event.filter));
+        }
+        if (this.tabTracking === 'tracking') {
+            this.p_t = event.page;
+            this.l_t = event.limit;
+            this.filter_t = fil;
+        } else {
+            this.p_e = event.page;
+            this.l_e = event.limit;
+            this.filter_e = fil;
+        }
+        this.search('tracking');
+    }
+
+    mergeTrackingEvent(event) {
+        this.trackingMerge.data_tracking_code = event.tracking_code;
+        this.trackingMerge.data_id = event.id;
+        this.mergeTracking.show();
+    }
+
+    getExtTrackings(type = 'search') {
+        let rs = this.tracks._ext;
+        if (this.trackingMerge.ext_tracking_code || this.trackingMergeSearch) {
+            const match = type === 'search' ? this.trackingMergeSearch : this.trackingMerge.ext_tracking_code;
+            rs = this.tracks._ext.filter(
+                c => c.tracking_code.toUpperCase().indexOf(match.toUpperCase()) !== -1);
+            console.log(rs);
+        }
+        return rs;
+    }
+
+    setTrackingTarget(id, tracking) {
+        this.trackingMerge.ext_tracking_code = tracking;
+        this.trackingMerge.ext_id = id;
+    }
+
+    exportExcel(id) {
+        this.packageService.get('us-sending/' + id).subscribe(
+            rs => {
+                if (rs.success) {
+                    this.popUp.success(rs.message);
+                    location.assign(rs.data.link);
+                } else {
+                    this.popUp.error(rs.message);
+                }
+            }
+        );
     }
 }
