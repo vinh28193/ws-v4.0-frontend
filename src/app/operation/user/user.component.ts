f0cccc0f5b79822d4908c50206eb2253d9779837
VienLV <VienLV@example.com>
2019-06-26 10:00:22 +0700

    fix user component

--- src/app/operation/user/user.component.ts
+++ src/app/operation/user/user.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {OperationDataComponent} from '../operation-data.component';
 import {OperationService} from '../operation.service';
+import {isArray} from 'util';
+import {ModalDirective} from 'ngx-bootstrap';
 
 @Component({
     selector: 'app-user',
@@ -8,11 +10,10 @@ import {OperationService} from '../operation.service';
     styleUrls: ['./user.component.css']
 })
 export class UserComponent extends OperationDataComponent implements OnInit {
-
     constructor(public http: OperationService) {
         super(http);
     }
-
+    @ViewChild(ModalDirective) AddUser: ModalDirective;
     public userSearchForm = {
         id: '',
         username: '',
@@ -22,6 +23,28 @@ export class UserComponent extends OperationDataComponent implements OnInit {
         active: '',
         limit: 20,
         page: 1,
+    };
+    public modelUser = {
+        id: '',
+        username: '',
+        email: '',
+        phone: '',
+        employee: '',
+        status: '',
+        remove: '',
+        type_customer: '',
+        facebook_acc_kit_id: '',
+        bm_wallet_id: '',
+        authAssigments: '',
+        first_name: '',
+        last_name: '',
+        locale: '',
+        store_id: '',
+        token_apn: '',
+        token_fcm: '',
+        uuid: '',
+        vip: '',
+        password: '',
     };
     public data: any;
 
@@ -38,9 +61,8 @@ export class UserComponent extends OperationDataComponent implements OnInit {
             }
         });
     }
-
     gettype(type) {
-        switch (type) {
+        switch (parseInt(type)) {
             case 1:
                 return 'Employee Ha Noi';
                 break;
@@ -54,5 +76,15 @@ export class UserComponent extends OperationDataComponent implements OnInit {
                 return 'Customer';
                 break;
         }
+    }
+
+    getScopes(assigments) {
+        let scopes = '';
+        if (assigments && assigments.length > 0 && isArray(assigments)) {
+            for (let i = 0; i < assigments.length; i++) {
+                scopes += i === 0 ? assigments[i].item_name : ' | ' + assigments[i].item_name;
+            }
+        }
+        return scopes;
     }
 }
