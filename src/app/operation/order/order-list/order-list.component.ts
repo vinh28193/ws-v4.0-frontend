6b20315ee6c32a313fedde0035c900aa0e37bbca
VienLV <VienLV@example.com>
2019-07-11 14:08:44 +0700

    fix purchasing

--- src/app/operation/order/order-list/order-list.component.ts
+++ src/app/operation/order/order-list/order-list.component.ts
@@ -1863,7 +1863,7 @@ export class OrderListComponent extends OrderDataComponent implements OnInit {
         return trackingCodes;
     }
     savePurchaseInfo(order) {
-        if (order.current_status === 'READY2PURCHASE') {
+        if (order.current_status === 'READY2PURCHASE' || order.current_status === 'PURCHASING') {
             order.purchase_order_id = this.purchaseOrder;
             order.purchase_transaction_id = this.purchaseTransaction;
         }
